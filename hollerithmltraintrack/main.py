5879330ae5a49e6b6f841201bf1ce2bb012fe430
lukasgro63 <76912397+lukasgro63@users.noreply.github.com>
2024-02-24 17:52:48 +0100

    Comments added

--- hollerithmltraintrack/main.py
+++ hollerithmltraintrack/main.py
@@ -6,8 +6,10 @@ class MLModelTrackerInterface:
         self.model_tracker = ModelTracker()
 
     def track_training(self, model, X_train, y_train, preprocessor):
-        # Starts the tracking process and returns the tracking result.
-        # Utilizes the track_model method from ModelTracker directly.
+        """
+        Starts the tracking process and returns the tracking result.
+        Utilizes the track_model method from ModelTracker directly.
+        """
         return self.model_tracker.track_model(model, X_train, y_train, preprocessor)
 
     def get_tracked_info(self):
