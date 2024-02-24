bf2191c3d2ba50038cf02869f6d52cfa4ecb907c
lukasgro63 <76912397+lukasgro63@users.noreply.github.com>
2024-02-24 12:43:14 +0100

    base main.py

---
+++ hollerithmltraintrack/main.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+
+class MLModelTracker:
+    """
+    A class to track and analyze the training process data of ML models
+    """
+
+    def __init__(self):
+        pass
+    
+    def track_model(self):
+        pass
+
+if __name__ == "__main__":
+    tracker = MLModelTracker()
+    
